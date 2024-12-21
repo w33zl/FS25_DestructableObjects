74b1646a9d3d4385cef405f4ed8a49ce25a5e7f4
w33zl <7383510+w33zl@users.noreply.github.com>
2024-12-21 13:08:57 +0100

    Add initialization needed for placeables loaded from savegame to be registered properly

--- FS25_DestructableObjects/DestructableObjects.lua
+++ FS25_DestructableObjects/DestructableObjects.lua
@@ -11,3 +11,22 @@ Changelog:
 
 --***heck https://github.com/w33zl/FS25_WeezlsModLib for details about the "Mod" class ***
 
+DestructiblePlaceableObjects = Mod:init()
+
+DestructiblePlaceableObjects:source("scripts/modLib/I3DHelper.lua")
+DestructiblePlaceableObjects:source("scripts/modLib/PlaceableExtension.lua")
+
+-- -- Event that is executed when your mod is loading (after the map has been loaded and before the game starts)
+-- function DestructiblePlaceableObjects:loadMap(filename)
+-- end
+
+-- -- Event that is continuously, USE WITH CAUTION! Any demanding code here (even just a simple "print()" command) will cause poor performance, stuttering and FPS drops
+-- function DestructiblePlaceableObjects:update(dt)
+-- end
+
+-- Event that is executed when the player chooses to start the mission (after the map has been loaded and before the game starts)
+function DestructiblePlaceableObjects:startMission()
+    --HACK: this is a bit of a hack, maybe we can find a better way to do this?
+    DestructibleMapObjectSystem.loadFromSavegameXML(g_currentMission.destructibleMapObjectSystem, g_currentMission.missionInfo.destructibleMapObjectsXMLLoad)
+end
+
