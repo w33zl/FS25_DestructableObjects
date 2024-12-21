24b4a689a1509dff17578526650df10382174a4e
w33zl <7383510+w33zl@users.noreply.github.com>
2024-12-21 13:06:10 +0100

    Initial commit

---
+++ FS25_DestructableObjects/DestructableObjects.lua
@@ -0,0 +1,13 @@
+--[[
+SHORT DESCRIPTION OF WHAT YOUR MOD DOES GOES HERE
+
+Author:     w33zl
+Version:    1.0.0
+Modified:   2024-12-07
+
+Changelog:
+
+]]
+
+--***heck https://github.com/w33zl/FS25_WeezlsModLib for details about the "Mod" class ***
+
