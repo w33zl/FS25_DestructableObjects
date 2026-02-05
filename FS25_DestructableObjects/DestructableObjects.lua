47448a8fcdc07cfe14d419494c0165985f69ad46
w33zl <7383510+w33zl@users.noreply.github.com>
2026-02-05 17:26:55 +0100

    Bugfix [#1]: Prevent warnings in the log ("Group with id 'n' does not exist in map") on maps with rock missions

--- FS25_DestructableObjects/DestructableObjects.lua
+++ FS25_DestructableObjects/DestructableObjects.lua
@@ -19,15 +19,31 @@ DestructiblePlaceableObjects:source("scripts/modLib/PlaceableExtension.lua")
 -- -- Event that is executed when your mod is loading (after the map has been loaded and before the game starts)
 -- function DestructiblePlaceableObjects:loadMap(filename)
 -- end
+Mission00.loadAdditionalFilesFinished = Utils.overwrittenFunction(Mission00.loadAdditionalFilesFinished, function(self, superFunc, ...)
+    local xmlFile = g_currentMission ~= nil and g_currentMission.missionInfo ~= nil and g_currentMission.missionInfo.destructibleMapObjectsXMLLoad
+    Log:var("g_currentMission.missionInfo.destructibleMapObjectsXMLLoad [loadAdditionalFilesFinished]", xmlFile)
+    Log:trace("loadAdditionalFilesFinished")
 
--- -- Event that is continuously, USE WITH CAUTION! Any demanding code here (even just a simple "print()" command) will cause poor performance, stuttering and FPS drops
--- function DestructiblePlaceableObjects:update(dt)
--- end
+    self.destructibleMapObjectsXMLLoad = xmlFile
+    g_currentMission.missionInfo.destructibleMapObjectsXMLLoad = nil
+
+    local retVal = superFunc(self, ...)
+
+    -- g_currentMission.missionInfo.destructibleMapObjectsXMLLoad = self.destructibleMapObjectsXMLLoad
+    
+
+    return retVal
+end)
+Mission00.onFinishedPlaceables = Utils.appendedFunction(Mission00.onFinishedPlaceables, function(self)
+	Log:trace("Mission00.onFinishedPlaceables")
+
+    DestructibleMapObjectSystem.loadFromSavegameXML(g_currentMission.destructibleMapObjectSystem, self.destructibleMapObjectsXMLLoad)
+end)
 
 -- Event that is executed when the player chooses to start the mission (after the map has been loaded and before the game starts)
 function DestructiblePlaceableObjects:startMission()
     --HACK: this is a bit of a hack, maybe we can find a better way to do this?
-    DestructibleMapObjectSystem.loadFromSavegameXML(g_currentMission.destructibleMapObjectSystem, g_currentMission.missionInfo.destructibleMapObjectsXMLLoad)
+    -- DestructibleMapObjectSystem.loadFromSavegameXML(g_currentMission.destructibleMapObjectSystem, self.destructibleMapObjectsXMLLoad)
 end
 
 g_globalMods = g_globalMods or {}
